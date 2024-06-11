1ea59b6775e17768eb0fbc907df37023a3e86789
Eduardo <eduardopires1102@gmail.com>
2024-06-11 13:25:39 -0300

    Initial commit

---
+++ src/JavaExercices/FirstProjectJava.java
@@ -0,0 +1,11 @@
+package JavaExercices;
+
+public class FirstProjectJava {
+    public static void main(String[] args) {
+        String nome = "Eduardo";
+        String endereco = "São José dos Campos-SP";
+        float salario = 1412.00f;
+        String dataSalario = "15/02/2024";
+        System.out.println("Eu " + nome + ", morando no endereço " + endereco + ",confirmo que recebi o salário de R$" + salario + ", na data " + dataSalario + ".");
+    }
+}
